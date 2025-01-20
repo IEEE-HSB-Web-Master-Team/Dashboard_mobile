d7c2c62274f1698f9ea5675ba1c11fa85cff0299
Elgaml <mahmoudelgmal991@gmail.com>
2025-01-21 01:43:54 +0200

    add test folder add screen utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:dash_board_ieee/config/routes/routs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      routerConfig: AppRoute.router,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'Flutter Demo',
+          routerConfig: AppRoute.router,
+        );
+      },
     );
   }
 }
