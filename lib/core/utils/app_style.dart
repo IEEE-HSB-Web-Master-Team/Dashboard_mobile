92e81627f59e5d9e7ed49f48effd12e4f53687f4
YasmeenGad <gadyasmeen335@gmail.com>
2025-01-21 23:47:05 +0200

    feat: create onBoarding View

--- lib/core/utils/app_style.dart
+++ lib/core/utils/app_style.dart
@@ -1,26 +1,52 @@
-
 import 'package:flutter/material.dart';
 
-class AppStyle {
-
-  static double scaleFactor(double width) {
-    if (width < 500) {
-      return width / 400;
-    } else if (width > 1200) {
-      return width / 1000;
-    } else {
-      return width / 300;
-    }
+abstract class AppStyles {
+  static TextStyle styleMostThick24(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontWeight: FontWeight.w900,
+    );
   }
 
-  static double responsiveFont(
-      {required double fontSize, required BuildContext context}) {
-    double width = MediaQuery.of(context).size.width;
-    double scalefactor = scaleFactor(width);
-    double lowerLimit = fontSize * .8;
-    double upperLimit = fontSize * 1.3;
+  static TextStyle styleMedium13(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 13),
+      fontWeight: FontWeight.w500,
+    );
+  }
 
-    double responsiveFont = fontSize * scalefactor;
-    return responsiveFont.clamp(lowerLimit, upperLimit);
+  static TextStyle styleMostThick18(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontWeight: FontWeight.w900,
+    );
+  }
+
+  static TextStyle styleMostThick16(context) {
+    return TextStyle(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w900,
+    );
+  }
+}
+
+double getResponsiveFontSize(context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+
+  double lowerLimit = fontSize * .8;
+  double upperLimit = fontSize * 1.2;
+
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width < 800) {
+    return width / 550;
+  } else if (width < 1200) {
+    return width / 1000;
+  } else {
+    return width / 1920;
   }
 }
