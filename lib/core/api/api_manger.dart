2ffabc0e14ad42a7dd480221b04b98a30d5e4b68
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-09 21:12:41 +0200

    feat: add login method in retrofit

--- lib/core/api/api_manger.dart
+++ lib/core/api/api_manger.dart
@@ -1,8 +1,10 @@
+import 'package:dash_board_ieee/features/Auth/data/models/response/login_response_dto.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/error_logger.dart';
 import 'package:retrofit/http.dart';
 
+import '../../features/Auth/data/models/request/login_request_dto.dart';
 import 'end_points.dart';
 
 part 'api_manger.g.dart';
@@ -13,6 +15,6 @@ abstract class NewApiManger {
   @factoryMethod
   factory NewApiManger(Dio dio) = _NewApiManger;
 
-// @POST(EndPoints.login)
-// Future<String> login(@Body() Map<String, dynamic> data);
+  @POST(EndPoints.login)
+  Future<LoginResponseDto> login(@Body() LoginRequestDto data);
 }
